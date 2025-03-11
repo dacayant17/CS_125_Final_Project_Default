b294cacd428ba5bc3b4b0634ebd44ed7f22fbfe8
dacayant17 <dacayant@my.erau.edu>
2025-03-11 11:11:02 -0700

    Create final_project_texas_hold_em.c

---
+++ final_project_texas_hold_em.c
@@ -0,0 +1,32 @@
+
+/*
+ * Author: Timothy Dacayanan
+ * Course: CS 125
+ * Assignment: Final Project- Texas Hold Em
+ * Date: 3/11/2025
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Card {
+  char suit;
+  int rank;
+  int inPlay
+} card;
+
+typedef struct Person {
+  card c1;
+  card c2;
+} Player;
+
+void 
+
+void printCard(char suit, char rank) {
+  printf("%c%c\n", )
+}
+
+int main(int argc, char *argv[]) {
+
+}
