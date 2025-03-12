66feb065a097e423fbabff20f99df7bd929f0565
dacayant17 <dacayant@my.erau.edu>
2025-03-12 16:25:00 -0700

    3/12/2025 4:25 texas hold em update

--- final_project_texas_hold_em.c
+++ final_project_texas_hold_em.c
@@ -13,7 +13,7 @@
 typedef struct Card {
   char suit;
   int rank;
-  int inPlay
+  int inPlay;
 } card;
 
 typedef struct Person {
@@ -21,12 +21,51 @@ typedef struct Person {
   card c2;
 } Player;
 
-void 
-
 void printCard(char suit, char rank) {
-  printf("%c%c\n", )
+  printf("%c", suit);
+  switch (rank) {
+    case (1):
+      printf("A\n");
+      break;
+    case (11):
+      printf("J\n");
+      break;
+    case (12):
+      printf("Q\n");
+      break;
+    case (13):
+      printf("K\n");
+      break;
+    default:
+      printf("%i", rank);
+      break;
+  }
 }
 
-int main(int argc, char *argv[]) {
+//int main(int argc, char *argv[]) {
+int main() {
+  //int in1 = atoi(argv[1]);
+  int in1 = 1000;
+  int i;
+  char suits[4] = {'D', 'H', 'S', 'C'};
+  card deck[52];
+  card community[5];
 
+  for (i = 0; i < 13; i++) {
+    deck[i].suit = suits[0];
+    deck[(i + 13)].suit = suits[1];
+    deck[(i + 26)].suit = suits[2];
+    deck[(i + 39)].suit = suits[3];
+    deck[i].rank = (i + 1);
+    deck[(i + 13)].rank = (i + 1);
+    deck[(i + 26)].rank = (i + 1);
+    deck[(i + 39)].rank = (i + 1);
+  }
+
+  for (i = 0; i < 52; i++) {
+    deck[i].inPlay = 0;
+  }
+  for (i = 0; i < 52; i++) {
+    printCard(deck[i].suit, deck[i].rank);
+  }
 }
